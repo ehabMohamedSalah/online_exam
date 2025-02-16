ba41f81208d005d44125d51d197495950b4d352e
ehab salah <ehabsalah52t@gmail.com>
2025-02-16 21:57:04 +0200

    test

--- lib/core/api/endpoints.dart
+++ lib/core/api/endpoints.dart
@@ -2,6 +2,7 @@ class EndPoint{
 
   static const String signInEndpoint="api/v1/auth/signin";
   static const String signUpEndpoint="api/v1/auth/signup";
-
+  static const String verifyEndpoint = "api/v1/auth/verifyResetCode";
+  static const String changePasswordEndpoint = "api/v1/auth/changePassword";
 }
  
--- lib/core/utils/routes_manager.dart
+++ lib/core/utils/routes_manager.dart
@@ -2,5 +2,7 @@ class RouteManager{
 static const String homeScreen="/HOMESCREEN";
 static const String loginScreen="/LOGINSCREEN";
 static const String signUpscreen="/SIGNUPSCREEN";
-
+static const String otpScreen = "/OTPSCREEN";
+static const String forgetPassScreen = "/FORGETPASSSCREEN";
+static const String emailScreen = "/EMAILSCREEN";
 }
