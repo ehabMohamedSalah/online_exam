c0ecaf00b7b91fd76c658acc2c62b6c01bb27a0b
ehab salah <ehabsalah52t@gmail.com>
2025-02-19 23:57:29 +0200

    feat(bottom-nav): complete implementation of bottom navigation bar

--- lib/presentation/home/view/homeSceen.dart
+++ lib/presentation/home/view/homeSceen.dart
@@ -1,16 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:online_exam/core/utils/colors_manager.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+import '../../../core/utils/assets_manager.dart';
+import '../../../core/utils/string_manager.dart';
+import '../tabs/explore_tab/explore_tab_screen.dart';
+import '../tabs/profile_tab/profile_tab_screen.dart';
+import '../tabs/result_tab/result_tab_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+    HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  int currentNavIndex=0;
+  List<Widget>navWidget=[
+    ExploreTabScreen(),
+    ResultTabScreen(),
+    ProfileTabScreen(),
+
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("hello,worldd"),
-        ],
-      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex:currentNavIndex , //eh al item ale t3mlo select
+           onTap: (index) {
+            setState(() {
+              currentNavIndex=index;
+            });
+
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetsManager.explore,
+                colorFilter: ColorFilter.mode(
+                  ColorManager.greyColor, // Default color for unselected item
+                  BlendMode.srcIn,
+                ),
+              ),
+              activeIcon: SvgPicture.asset(
+                AssetsManager.explore,
+                colorFilter: ColorFilter.mode(
+                  ColorManager.primaryColor, // Color for selected item
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: StringManager.explore,
+            ),
+
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetsManager.result,
+                colorFilter: ColorFilter.mode(
+                  ColorManager.greyColor, // Default color for unselected item
+                  BlendMode.srcIn,
+                ),
+              ),
+              activeIcon: SvgPicture.asset(
+                AssetsManager.result,
+                colorFilter: ColorFilter.mode(
+                  ColorManager.primaryColor, // Color for selected item
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: StringManager.result,
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                AssetsManager.profile,
+                colorFilter: ColorFilter.mode(
+                  ColorManager.greyColor, // Default color for unselected item
+                  BlendMode.srcIn,
+                ),
+              ),
+              activeIcon: SvgPicture.asset(
+                AssetsManager.profile,
+                colorFilter: ColorFilter.mode(
+                  ColorManager.primaryColor, // Color for selected item
+                  BlendMode.srcIn,
+                ),
+              ),
+              label: StringManager.profile,
+            ),
+          ]),
+
+      //kda al body howa al hytghyr m3 al bottom navigation y3ny tghyer fe al index
+      body: navWidget[currentNavIndex],
     );
   }
 }
